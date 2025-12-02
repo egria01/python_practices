f9bc721b5fbcb6ecb155ddfefa0eeb8cdab09284
egria01 <nzclee@gmail.com>
2025-12-02 20:29:44 +0800

    first commit

---
+++ py/chap1a.py
@@ -0,0 +1,36 @@
+
+name = "Bob"        #Striong
+age = 10            #Integer
+weight = 20.5       #Float
+is_female = False   #Boolean
+
+#print("Name:", name)
+#print(type(name))
+
+x = 10
+y = 3
+print(x + y)    #Addition  
+print(x - y)    #Subtraction
+print(x * y)    #Multiplication
+print(x / y)    #Division
+print(x // y)   #Floor Division
+print(x % y)    #Modulus
+print(x ** y)   #Exponentiation
+
+print("var1 = Hello"
++" var2 = 10 "+ "var3 = 5.5 "+ "var4 = True")
+var1 = "Hello"
+var2 = 10
+var3 = 5.5
+var4 = True
+
+print(var1+" is of type: "+ str(type(var1)))
+print(str(var2)+" is of type: "+ str(type(var2)))
+print(str(var3)+" is of type: "+ str(type(var3)))
+print(str(var4)+" is of type: "+ str(type(var4)))
+
+Celsius = 25
+print("Fahrenheit= (Celcius * 9/5) + 32")
+Fahrenheit = (Celsius * 9/5) + 32
+
+print("Fahrenheit=", Fahrenheit)
